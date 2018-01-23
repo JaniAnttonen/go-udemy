feae921850b9be0426d3a9a3cef96bf989236733
Jani Anttonen <jantto@utu.fi>
2018-01-23 12:04:38 +0200

    use correct go path, linting

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 const (
-  text1 = "Lorem ipsum dolor ame ."
-  text2 = "Lorem dolor sit amet. "
+	text1 = "Lorem ipsum dolor ame ."
+	text2 = "Lorem dolor sit amet. "
 )
 
 func main() {
-  fmt.Println(text1, text2)
+	fmt.Println(text1, text2)
 }
